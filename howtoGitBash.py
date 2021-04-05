4ea33b1d3fe4cd0eaaf4d4c5111981e17dc72b09
Colin Bumpass <cmb153@latech.edu>
2021-04-05 00:45:33 -0500

    Maybe this'll help

--- howtoGitBash.py
+++ howtoGitBash.py
@@ -1,7 +1,15 @@
 # This is just a text file. Remembering how to make files via Git Bash.
 """
 I don't really remember the specifics of Git Bash and Git Hub, so I apologize
-if I accidentally misunderstand terms or the process of editting/saving a file.
+if I accidentally misunderstand terms of the process of editting/saving a file.
+
+Fun fact, this py file was once named "t2.py" but I changed the name to 
+"howtoGitBash.py." This created two of the same files, but with different names,
+in the repository. Even after deleting t2.py from my own folder, it still existed
+in the repository. So, to delete a file from the repository, hit the trashcan icon
+at the top right of the code, and hit "commit changes" at the bottom to confirm
+that you are removing a file from the repository.
+
 - Colin 4/5
 """
 
