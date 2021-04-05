9412a17186c26710a7f8ba0fdeaba809aa803c1b
Colin Bumpass <cmb153@latech.edu>
2021-04-05 00:37:11 -0500

    how

---
+++ howtoGitBash.py
@@ -0,0 +1,20 @@
+# This is just a text file. Remembering how to make files via Git Bash.
+"""
+I don't really remember the specifics of Git Bash and Git Hub, so I apologize
+if I accidentally misunderstand terms or the process of editting/saving a file.
+- Colin 4/5
+"""
+
+
+# To make a py file: touch INSERT_NAME.py
+# (after you've opened up Git Bash in the folder) 
+
+# To pull a py file: git pull origin main
+# Pulls all files from the repository 
+
+# To add your editted file: git add INSERT_NAME.py
+
+# To commit your file to the repository on Git Hub: git commit -m "INSERT TEXT"
+
+# To finally push your progress to the repository: git push origin main
+
