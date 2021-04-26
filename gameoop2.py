f79766db9dd0dd068cfb524ba3edbc9442ff89ea
Colin Bumpass <cmb153@latech.edu>
2021-04-25 19:56:40 -0500

    shooty

--- gameoop2.py
+++ gameoop2.py
@@ -1,25 +1,29 @@
 """
 COLIN: So far this version of our project contains:
-- Player movement to the left and right
-- Function for the player firing red projectiles upwards
+- Player movement
 - A player sprite
 - A type of enemy sprite
+- basic projectile and collision
 COLIN: But it also lacks:
 - A main menu with game options
 - Player lives {and health?}
+- Co-op and Versus???
 - Enemy variations, movements, attacks, spawning, and health
-- Music for menu and game
+- Music for menu?git 
 - Arcade button compatibility
 - A box to put it all in
+
 """
 
 # Import pygame
 import pygame
+import time
 
 # Initialize the pygame module
 pygame.init()
 
-#=====[ IMAGES ]=====
+
+#=====[ IMAGES & AUDIO ]=====
 # COLIN: Here, we can put other images for enemies and a possible player 2
 # Grabs image for the player sprite
 PLAYER = pygame.image.load("GalagaShip.png")
@@ -31,6 +35,10 @@ BUG1 = pygame.transform.scale(BUG1, (55, 40))
 
 # Grab image for background
 BG = pygame.image.load("space.jpg")
+
+# Grab music for game audio
+MUSIC = pygame.mixer.music.load("game_music.mp3")
+FIRE = pygame.mixer.Sound("laserfire.mp3")
 
 #=====[ CREATE WINDOW ]======
 # Constant variable for window's width (800) and height (400); RPi screen size
@@ -52,21 +60,24 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         # Call the fancy pygame sprite parent class
         super().__init__()
-        # instance variable for player image from
+        # instance variable to grab player image
         self.image = PLAYER
+        # Creates the hitbox and tracks coordinates for the player
         self.rect = self.image.get_rect()
-        # instance variable for x coordinate
 
-
+    # Function that moves the player to the right when called
     def moveRight(self, pixels):
         self.rect.x += pixels
 
+    # Function that moves the player to the left when called
     def moveLeft(self, pixels):
         self.rect.x -= pixels
 
+    # Function that moves the player upwards when called
     def moveUp(self, pixels):
         self.rect.y -= pixels
 
+    # Function that moves the player downwards when called
     def moveDown(self, pixels):
         self.rect.y += pixels
 
@@ -74,57 +85,58 @@ class Player(pygame.sprite.Sprite):
     def draw(self,window):
         window.blit(self.image, (self.rect.x,self.rect.y))
 
-#------[ BUG TYPE 1 CLASS ]------
-class Bug1(pygame.sprite.Sprite):
+# Class for the laser sprite
+class Laser(pygame.sprite.Sprite):
     def __init__(self):
+        # Calls pygame's sprite class
         super().__init__()
-        self.image = BUG1
+        # Creates a rectangle with width and height of 4 and fills it with predefined color RED
+        self.image = pygame.Surface([4,4])
+        self.image.fill(RED)
+        # Creates a rectangle (hitbox) for the sprite and tracks its coordinates
         self.rect = self.image.get_rect()
-        self.health = 1
 
+    # Updates the sprite's movements
+    def update(self):
+        # Move the sprite up
+        self.rect.y -= 7
+
+#------[ BUG TYPE 1 CLASS ]------
+class BugT1(pygame.sprite.Sprite):
+    def __init__(self):
+        # Calls pygame's sprite class
+        super().__init__()
+        # Grabs the predefined variable from up top that represents the bug's image
+        self.image = BUG1
+        # Creates rectangle for the sprite and tracks coordinates
+        self.rect = self.image.get_rect()
+        # Instance variable that acts as a boolean for the for loop below
+        self.move = 1
+
+    # Function that draws the Bug Type 1 onto the screen at given coordinates
     def draw(self, window):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-    def mov(self):
-        for i in range(self.health):
+    # Update the sprite's movement
+    def update(self):
+        for i in range(self.move):
+            #print("x = {}".format(self.rect.x))
+            #print("y = {}".format(self.rect.y))
             if self.rect.x != 745 and self.rect.y == 0:
-                print("x = {}".format(self.rect.x))
-                print("y = {}".format(self.rect.y))
                 self.rect.x += 5
             if self.rect.x == 745 and self.rect.y < 40:
-                print("x = {}".format(self.rect.x))
-                print("y = {}".format(self.rect.y))
                 self.rect.y += 5
-                self.health -= 1
             if self.rect.x != 0 and self.rect.y == 40:
-                print("x = {}".format(self.rect.x))
-                print("y = {}".format(self.rect.y))
                 self.rect.x -= 5
             if self.rect.x == 0 and self.rect.y < 80:
-                print("x = {}".format(self.rect.x))
-                print("y = {}".format(self.rect.y))
                 self.rect.y += 5
             if self.rect.x < 745 and self.rect.y == 80:
-                print("x = {}".format(self.rect.x))
-                print("y = {}".format(self.rect.y))
                 self.rect.x += 5
-
-
-# Standalone function for the player's projectiles
-def projectiles(player_proj):
-    # For every instance of a projectile added to the list of projectiles
-    for proj in player_proj:
-        # Send a projectile 7 pixels up
-        proj.y -= 7
-
 
 #=====[ MAIN GAME LOOP ]=====
 # COLIN: Here is where the game is run. It sets up the foundation of the pygame, then refers to several
 # classes and functions to display the pygame.
 def main():
-    player_proj = []
-    # This is a counter for the amount of waves the player has gone through
-    wave = 0
     # Run set to True so the game doesn't end
     run = True
     # Sets the Frame Rate
@@ -132,17 +144,26 @@ def main():
     # Creates a clock that makes sure computer runs program at desired FPS
     clock = pygame.time.Clock()
 
+    #-----[ SPRITES ]-----
+    # This creates a pygame group for (A)LL (SP)RITES
+    asp = pygame.sprite.Group()
+    # This creates a pygame group for enemies
+    enemy  = pygame.sprite.Group()
+    # This creates a pygame group for the player's projectiles
+    lasers = pygame.sprite.Group()
     # create player at desired coordinates (400, 360)
     player = Player()
+    asp.add(player)
     player.rect.x = 400
     player.rect.y = 360
     # Testing the bugs spawn at (0,0)
-    b1 = Bug1()
+    b1 = BugT1()
+    asp.add(b1)
+    enemy.add(b1)
     b1.rect.x = 0
     b1.rect.y = 0
 
-    enemy = pygame.sprite.Group()
-    enemy.add(b1)
+
     # This function, nested into the main loop, displays everything
     def display():
         # Blit (draw an object) the background at coordinates (0,0)
@@ -150,23 +171,25 @@ def main():
         # Refreshes the surfaces that pygame is displaying
         player.draw(WIN)
 
-        # For every instance of a projectile in the player's list of projectiles:
-        for proj in player_proj:
-            # draw it as a red rectangle
-            pygame.draw.rect(WIN, RED, proj)
-
-        if wave == 0:
-            b1.draw(WIN)
-            b1.mov()
+        # Draws all of the sprites in the ALL SPRITES group
+        asp.draw(WIN)
+        # Calls the update function from any sprites in the ALL SPRITES group
+        asp.update()
         # Updates the window
         pygame.display.update()
 
+    def music():
+        MUSIC_FILE = ("game_music.mp3")
+        pygame.mixer.init()
+        pygame.mixer.music.load(MUSIC_FILE)
+        pygame.mixer.music.play()
+
+    music()
     # Game loop for key controls
     while run:
+        # Sets the game to reset at the desired frames per second
         clock.tick(FPS)
-        # Calls the above display function to update the pygame's surfaces
 
-        display()
         # Player closes window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -175,17 +198,29 @@ def main():
             # Player presses left control to fire (will be fixed with arcade GPIO)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    # print("FIRE!\n") COLIN: This was just to test to make sure the player was firing
+                    # Set laser variable to the Laser class
+                    laser = Laser()
+                    # Set laser to appear from player's coordinates
+                    laser.rect.x = player.rect.x + 24
+                    laser.rect.y = player.rect.y
+                    # Add laser to the ALL SPRITES group as it's made
+                    asp.add(laser)
+                    # Add laser to the Lasers group as it's made
+                    lasers.add(laser)
+                    # Troubleshooting: Print Statement to Track if the spacebar is allowing player to fire
+                    # print("Fire!")
+                    pygame.mixer.Sound.play(FIRE)
 
-                    # Projectile is set to be inside the player's x image and have a width of 5 and height of 5
-                    proj = pygame.Rect(player.rect.x + 55 / 2, player.rect.y, 5, 5)
-                    # Projectile is added to the player's list of projectiles
-                    player_proj.append(proj)
+        # Collision between Player's lasers and enemies
+        for laser in lasers:
+            # Create a list that removes laser and enemy once they collide
+            enemyhit = pygame.sprite.spritecollide(laser, enemy, True)
+            for laser in enemyhit:
+                lasers.remove(laser)
+                asp.remove(laser)
+                print("HIT!")
 
-        collision = pygame.sprite.spritecollide(player, enemy, False)
-        for b1 in collision:
-            pygame.sprite.Sprite.kill(b1)
-
+        # Tracks keys being pressed
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_a] and player.rect.x - PIX > 0:
             player.moveLeft(PIX)
@@ -196,8 +231,8 @@ def main():
         if keys_pressed[pygame.K_s] and player.rect.y < 360:
             player.moveDown(PIX)
 
-        # Calls the projectiles function from the main loop
-        projectiles(player_proj)
+        #Calls the display function
+        display()
 
 #=====[ LAUNCH GAME ]=====
 # Calls main loop
